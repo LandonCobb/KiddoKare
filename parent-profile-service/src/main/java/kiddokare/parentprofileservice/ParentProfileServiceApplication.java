fb3dc68741b228e8ab96febd818f7fb204705aa8
Memesterr <98419522+Memesterr@users.noreply.github.com>
2023-05-08 10:43:32 -0600

    parent-profile-service Created

---
+++ parent-profile-service/src/main/java/kiddokare/parentprofileservice/ParentProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package kiddokare.parentprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentProfileServiceApplication.class, args);
+	}
+
+}
---
+++ parent-profile-service/src/test/java/kiddokare/parentprofileservice/ParentProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kiddokare.parentprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
