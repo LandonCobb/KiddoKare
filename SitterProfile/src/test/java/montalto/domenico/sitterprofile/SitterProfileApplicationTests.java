9dc86e2050071e9bed3b8599b35109ba4dcc9762
Domenico Montalto <montaltodomenico92@gmail.com>
2023-05-08 10:39:44 -0600

    SitterProfile created

---
+++ SitterProfile/src/test/java/montalto/domenico/sitterprofile/SitterProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package montalto.domenico.sitterprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitterProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
