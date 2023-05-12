2b76a63fb87283b1ed494a5132fef508838922a0
Domenico Montalto <montaltodomenico92@gmail.com>
2023-05-12 13:52:57 -0600

    Sitter Rest controller partially done

--- services/SitterProfile/src/main/java/montalto/domenico/sitterprofile/SitterRepository.java
+++ services/SitterProfile/src/main/java/montalto/domenico/sitterprofile/SitterRepository.java
@@ -1,2 +1,9 @@
-package montalto.domenico.sitterprofile;public interface SitterRepository {
+package montalto.domenico.sitterprofile;
+import java.util.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface SitterRepository extends MongoRepository<Sitter, UUID> {
+
+    public List<Sitter> findByNameContaining(String txt);
+
 }
--- services/SitterProfile/src/main/java/montalto/domenico/sitterprofile/SitterRestController.java
+++ services/SitterProfile/src/main/java/montalto/domenico/sitterprofile/SitterRestController.java
@@ -1,2 +1,54 @@
-package montalto.domenico.sitterprofile;public class SitterRestController {
+package montalto.domenico.sitterprofile;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+//change the port number with the port of our frontend
+//@CrossOrigin("http://localhost:8090")
+@RestController
+@RequestMapping("/sitter")
+public class SitterRestController {
+
+
+    //this is an instance of the mongoDB
+    @Autowired
+    private SitterRepository sitterRepo;
+
+    //GET
+    //localhost:8081/sitter
+    @GetMapping(path = "")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Sitter> findAllSitters() {
+        return sitterRepo.findAll();
+    }
+
+    //GET
+    //localhost:8081/sitter/{name}
+    @GetMapping(path = "/search/byName/{name}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Sitter> searchSitterByName(@PathVariable(required = true) String name){
+        return sitterRepo.findByNameContaining(name);
+    }
+
+    //GET
+    //localhost:8081/sitter/{sitterUUID} (YOU NEED A sitterUUID)
+    @GetMapping(path = "/search/byUUID/{sitterUUID}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Sitter searchSitterByUUID(@PathVariable UUID sitterUUID){
+
+        return sitterRepo.findById(sitterUUID).orElseThrow(() -> new NoSuchElementException());
+    }
+
+    //Post
+    //localhost:8081/sitter  (YOU NEED A BODY WITH A SITTER OBJ)
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createSitter(@RequestBody Sitter sitter) {
+        sitter.setSitterUUID(UUID.randomUUID());
+        sitterRepo.save(sitter);
+    }
 }
