ad22d5eef13b428e9d140c9c21a7b1c55784fff2
Memesterr <98419522+Memesterr@users.noreply.github.com>
2023-05-12 16:12:39 -0600

    Finished rest controller

--- services/parent-profile-service/src/main/java/kiddokare/parentprofileservice/ParentRestController.java
+++ services/parent-profile-service/src/main/java/kiddokare/parentprofileservice/ParentRestController.java
@@ -3,9 +3,11 @@ package kiddokare.parentprofileservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,6 +21,10 @@ public class ParentRestController {
     @PostMapping("")
     @ResponseStatus(code = HttpStatus.CREATED)
     public void createParentProfile(@RequestBody Parent parent) {
+        if (parentRepo.existsByEmailContaining(parent.getEmail())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Email is already registered!");
+        }
+
         parent.setParentUUID(UUID.randomUUID()); // Generate UUID
         parent.setCreationDate(LocalDate.now()); // Generate creation date
         parentRepo.save(parent); // Save parent profile
@@ -31,23 +37,52 @@ public class ParentRestController {
         return parentRepo.findAll();
     }
 
-    // Update
-    @PatchMapping("/{parentUUID}")
+    @GetMapping("/byEmail")
     @ResponseStatus(code = HttpStatus.OK)
-    public void updateParentProfile(@PathVariable(required = true) UUID parentUUID) {
-        Optional<Parent> oldParent = parentRepo.findById(parentUUID);
+    public Parent getParentProfileByEmail(@RequestHeader("X-Email") String email) {
+        return parentRepo.findByEmailContaining(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Parent does not exist!"));
+    }
 
-        if (oldParent.isPresent()) {
+    // Update
+    @PatchMapping("")
+    @ResponseStatus(code = HttpStatus.OK)
+    public void updateParentProfile(@RequestHeader("X-Email") String email, @RequestBody Parent newParent) {
+        Optional<Parent> requestedParent = parentRepo.findByEmailContaining(email); // Search for the old parent
+        if (requestedParent.isPresent()) {
+            Parent oldParent = requestedParent.get(); // Store old parent
 
+            // If the new parent body is missing a field, it will replace it with old parent body's field
+            if (newParent.getName() == null) {
+                newParent.setName(oldParent.getName());
+            }
+
+            if (newParent.getPassword() == null) {
+                newParent.setPassword(oldParent.getPassword());
+            }
+
+            if (newParent.getEmail() == null) {
+                newParent.setEmail(oldParent.getEmail());
+            }
+
+            if (newParent.getAddress() == null) {
+                newParent.setAddress(oldParent.getAddress());
+            }
+
+            newParent.setParentUUID(oldParent.getParentUUID()); // UUID should never change
+            newParent.setCreationDate(oldParent.getCreationDate()); // Creation date should never change
+
+            parentRepo.save(newParent);
         } else {
-
+            throw new NoSuchElementException(
+                    "Parent does not exist!"
+            );
         }
     }
 
     // Delete
     @DeleteMapping("")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void deleteParentProfile() {
-
+    public void deleteParentProfile(@RequestHeader("X-Email") String email) {
+        parentRepo.deleteByEmailContaining(email);
     }
 }
