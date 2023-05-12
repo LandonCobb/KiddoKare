4e7d9c5f89679f76b82a80ee884e75ebd5cee1cb
Memesterr <98419522+Memesterr@users.noreply.github.com>
2023-05-12 13:57:25 -0600

    Created branch, added endpoints to rest controller

--- services/parent-profile-service/src/main/java/kiddokare/parentprofileservice/ParentRestController.java
+++ services/parent-profile-service/src/main/java/kiddokare/parentprofileservice/ParentRestController.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/parent")
 public class ParentRestController {
@@ -13,22 +18,30 @@ public class ParentRestController {
     // Create
     @PostMapping("")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public void createParentProfile() {
-
+    public void createParentProfile(@RequestBody Parent parent) {
+        parent.setParentUUID(UUID.randomUUID()); // Generate UUID
+        parent.setCreationDate(LocalDate.now()); // Generate creation date
+        parentRepo.save(parent); // Save parent profile
     }
 
     // Find
     @GetMapping("")
     @ResponseStatus(code = HttpStatus.OK)
-    public void getParentProfile() {
-
+    public List<Parent> getAllParentProfiles() {
+        return parentRepo.findAll();
     }
 
     // Update
-    @PatchMapping("")
+    @PatchMapping("/{parentUUID}")
     @ResponseStatus(code = HttpStatus.OK)
-    public void updateParentProfile() {
+    public void updateParentProfile(@PathVariable(required = true) UUID parentUUID) {
+        Optional<Parent> oldParent = parentRepo.findById(parentUUID);
 
+        if (oldParent.isPresent()) {
+
+        } else {
+
+        }
     }
 
     // Delete
