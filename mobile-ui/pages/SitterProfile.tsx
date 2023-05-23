e82e1f5f18d00547445d687fe46ca76e31fdf1b9
Domenico Montalto <montaltodomenico92@gmail.com>
2023-05-23 13:55:00 -0600

    sitter mobile betta

--- mobile-ui/pages/SitterProfile.tsx
+++ mobile-ui/pages/SitterProfile.tsx
@@ -1,9 +1,55 @@
-import { Text, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Text, SafeAreaView, Button, TextInput } from "react-native";
 
 const SitterProfile = () => {
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+
+
+
     return (
         <SafeAreaView>
-            <Text>Sitter Profile</Text>
+            <Text>Email</Text>
+
+            <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={setEmail}
+        value={email}
+    />
+            <Text>Name</Text>
+
+            <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={setName}
+        value={name}
+    />
+
+            <Text>address</Text>
+
+            <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={setAddress}
+        value={address}
+    />
+
+            <Text>Password</Text>
+
+            <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={setPassword}
+        value={password}
+    />
+
+
+
+            <Button title="Sign Up"></Button>
+
         </SafeAreaView>
     );
 };
