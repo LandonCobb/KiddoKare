63d2b80ceadc2fe4eb21bdf9335e9f0c3b77c26b
LandonCobb <landoncobb123@gmail.com>
2023-05-26 10:06:09 -0600

    added kafka producer

--- services/schedule/models/schedule.js
+++ services/schedule/models/schedule.js
@@ -1,20 +1,46 @@
 import mongoose from "mongoose";
 
-const {Schema, model, ObjectId} = mongoose;
+const { Schema, model, ObjectId } = mongoose;
 
-const scheduleSchema = new Schema({
+const blockSchema = new Schema({
+  blockId: String,
+  startHour: {
+    type: Number,
+    range: [0, 23],
+  },
+  endHour: {
+    type: Number,
+    range: [0, 23],
+  },
+  dayOfWeek: {
+    type: String,
+    // prettier-ignore
+    enum: [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ],
+  },
+  dateBooked: {
+    type: Date,
+    default: null,
+  },
+  bookedParentId: {
+    type: ObjectId,
+    default: null,
+  },
+});
+
+const scheduleSchema = new Schema(
+  {
     blocks: {
-        type: [{ 
-            blockId: String,
-            startHour: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 0,
-            endHour: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 0,
-            dayOfWeek: "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday",
-            dateBooked: Date | null,
-            bookedParentId: ObjectId | null,
-          }],
-        required: [true, "schedule list is required"],
-        default: []
+      type: [blockSchema],
+      required: [true, "schedule list is required"],
+      default: [],
+    },
+    sitterEmail: {
+        type: String,
+        required: [true, "sitter's email is required"],
+        default: "no sitter email found"
     }
-}, {timestamps: true})
+  },
+  { timestamps: true }
+);
 
 export const ScheduleModel = model("SCHEDULE", scheduleSchema);
