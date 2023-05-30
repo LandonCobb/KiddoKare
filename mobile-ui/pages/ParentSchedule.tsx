68bba3a6aaace00973206d55ffc5599d7b7b580d
Domenico Montalto <montaltodomenico92@gmail.com>
2023-05-30 13:42:06 -0600

    Added styling to parent and sitter schedule

--- mobile-ui/pages/ParentSchedule.tsx
+++ mobile-ui/pages/ParentSchedule.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import{Text, SafeAreaView, View} from "react-native";
+import{ StyleSheet, Text, SafeAreaView, View } from "react-native";
 import ip from "../ip";
 import { SecurityContext } from "../contexts/SecurityProvider";
 import { ScrollView } from "react-native-gesture-handler";
@@ -28,14 +28,16 @@ const ParentSchedule = () => {
 
 
     return(
-        <SafeAreaView>
-            <ScrollView>
+        <SafeAreaView style={styles.FormContainer}>
+            <ScrollView style={styles.ScrollView}>
                 {timeBlocks && (
                     timeBlocks.map((block) => (
                         <View>
-                            <Text>Start Hour: {block.startHour}</Text>
-                            <Text>End Hour: {block.endHour}</Text>
-                            <Text>Date: {block.dateBooked.split(":")[0].slice(0,-3)}</Text>
+                            <Text style={styles.TextHeader2}>Date: {block.dateBooked.split(":")[0].slice(0,-3)}</Text>
+                            <View style={styles.ResultContainer}>
+                                <Text style={styles.Text}>Start Hour: {block.startHour}</Text>
+                                <Text style={styles.Text}>End Hour: {block.endHour}</Text>
+                            </View>
                         </View>
                     ))
                 )}
@@ -43,5 +45,82 @@ const ParentSchedule = () => {
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    FormContainer: {
+        flex: 1,
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        backgroundColor: "#212121",
+    },
+
+    TextInputsContainer: {
+        justifyContent: "center",
+        alignItems: "stretch",
+        borderRadius: 5,
+        marginLeft: "5%",
+        marginTop: "5%",
+        width: "75%",
+    },
+
+    ButtonContainer: {
+        marginLeft: "5%",
+        marginTop: "5%",
+    },
+
+    RowContainer: {
+        flexDirection: "row",
+    },
+
+    ResultContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        alignContent: "center",
+        padding: "2%",
+        backgroundColor: "#212121",
+        borderRadius: 5,
+        marginBottom: "3%",
+    },
+
+    ScrollView: {
+        borderRadius: 5,
+        backgroundColor: "#101010",
+        padding: "3%",
+        marginBottom: "5%",
+        marginTop: "5%",
+        width: "95%",
+        height: "80%"
+    },
+
+    TextHeader: {
+        fontSize: 48,
+        fontWeight: "300",
+        color: "#ffffff",
+    },
+
+    TextHeader2: {
+        fontSize: 24,
+        fontWeight: "500",
+        color: "#ffffff",
+    },
+
+    Text: {
+        marginBottom: "0.5%",
+        fontSize: 20,
+        fontWeight: "500",
+        color: "#a0a0a0",
+    },
+
+    TextInput: {
+        alignItems: "stretch",
+        borderRadius: 5,
+        padding: "3%",
+        fontSize: 24,
+        color: "#ffffff",
+        backgroundColor: "#181818",
+    },
+});
+const buttonColor = "#9e00ff";
 
 export default ParentSchedule;
--- mobile-ui/pages/SitterSchedule.tsx
+++ mobile-ui/pages/SitterSchedule.tsx
@@ -1,6 +1,6 @@
-import{Text, SafeAreaView, View} from "react-native";
+import{ Text, SafeAreaView, View, StyleSheet } from "react-native";
 import TimeBlock from "../types/TimeBlock";
-import {useEffect, useState, useContext} from "react";
+import { useEffect, useState, useContext } from "react";
 import ip from "../ip";
 import { ScheduleIdContext } from "../contexts/ScheduleIdProvider";
 import { ScrollView } from "react-native-gesture-handler";
@@ -23,36 +23,38 @@ const SitterSchedule = () => {
         } );
     }, []);
 
-
-
-
     return(
-        <SafeAreaView>
+        <SafeAreaView style={styles.FormContainer}>
             {timeBlocks && (
                 <View>
-                    <View>
-                        <Text>Schedule</Text>
-                        <ScrollView>
+                    <View style={{height: "48%"}}>
+                        <Text style={styles.TextHeader}>Schedule</Text>
+                        <ScrollView style={styles.ScrollView}>
                             {timeBlocks.blocks.map((block) => (
                                 !block.dateBooked && (
                                     <View>
-                                        <Text>Start Hour: {block.startHour}</Text>
-                                        <Text>End Hour: {block.endHour}</Text>
-                                        <Text>{block.dayOfWeek}</Text>
-                                    </View>)
+                                        <Text style={styles.TextHeader2}>{block.dayOfWeek}:</Text>
+                                        <View style={styles.ResultContainer} >
+                                            <Text style={styles.Text}>Start Hour: {block.startHour}</Text>
+                                            <Text style={styles.Text}>End Hour: {block.endHour}</Text>
+                                        </View>
+                                    </View>
+                                    )
                             ))}
                         </ScrollView>
                     </View>
-                    <View>
-                        <Text>Reserved</Text>
-                        <ScrollView>
+                    <View style={{height: "48%"}}>
+                        <Text style={styles.TextHeader}>Reserved</Text>
+                        <ScrollView style={styles.ScrollView}>
                         {timeBlocks.blocks.map((block) => (
                                 block.dateBooked && (
                                     <View>
-                                        <Text>Start Hour: {block.startHour}</Text>
-                                        <Text>End Hour: {block.endHour}</Text>
-                                        <Text>{block.dayOfWeek}</Text>
-                                        <Text>Date: {block.dateBooked.split(":")[0].slice(0,-3)}</Text>
+                                        <Text style={styles.TextHeader2}>{block.dayOfWeek}:</Text>
+                                        <View style={styles.ResultContainer}>
+                                            <Text style={styles.Text}>Date: {block.dateBooked.split(":")[0].slice(0,-3)}</Text>
+                                            <Text style={styles.Text}>Start Hour: {block.startHour}</Text>
+                                            <Text style={styles.Text}>End Hour: {block.endHour}</Text>
+                                        </View>
                                     </View>
                                 )
                             ))}
@@ -63,6 +65,81 @@ const SitterSchedule = () => {
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    FormContainer: {
+        flex: 1,
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        backgroundColor: "#212121",
+    },
+
+    TextInputsContainer: {
+        justifyContent: "center",
+        alignItems: "stretch",
+        borderRadius: 5,
+        marginLeft: "5%",
+        marginTop: "5%",
+        width: "75%",
+    },
+
+    ButtonContainer: {
+        marginLeft: "5%",
+        marginTop: "5%",
+    },
+
+    RowContainer: {
+        flexDirection: "row",
+    },
+
+    ResultContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        alignContent: "center",
+        padding: "2%",
+        backgroundColor: "#212121",
+        borderRadius: 5,
+        marginBottom: "3%",
+    },
+
+    ScrollView: {
+        borderRadius: 5,
+        backgroundColor: "#101010",
+        padding: "3%",
+        marginBottom: "5%",
+        width: "95%",
+    },
+
+    TextHeader: {
+        fontSize: 48,
+        fontWeight: "300",
+        color: "#ffffff",
+    },
+
+    TextHeader2: {
+        fontSize: 24,
+        fontWeight: "500",
+        color: "#ffffff",
+    },
+
+    Text: {
+        marginBottom: "0.5%",
+        fontSize: 20,
+        fontWeight: "500",
+        color: "#a0a0a0",
+    },
+
+    TextInput: {
+        alignItems: "stretch",
+        borderRadius: 5,
+        padding: "3%",
+        fontSize: 24,
+        color: "#ffffff",
+        backgroundColor: "#181818",
+    },
+});
+const buttonColor = "#9e00ff";
 
 export default SitterSchedule;
 
