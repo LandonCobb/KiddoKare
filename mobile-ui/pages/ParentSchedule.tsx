a4ec77649f99a4bd953aa21f20b71eb6580718a0
Domenico Montalto <montaltodomenico92@gmail.com>
2023-05-30 12:15:54 -0600

    ParentSchedule working without design

--- mobile-ui/pages/ParentSchedule.tsx
+++ mobile-ui/pages/ParentSchedule.tsx
@@ -1,13 +1,49 @@
-import{Text, SafeAreaView} from "react-native";
+import { useContext, useEffect, useState } from "react";
+import{Text, SafeAreaView, View} from "react-native";
+import ip from "../ip";
+import { SecurityContext } from "../contexts/SecurityProvider";
+import { ScrollView } from "react-native-gesture-handler";
+
+interface TimeBlock {
+    startHour: number;
+    endHour: number;
+    dayOfWeek: string;
+    dateBooked: string;
+    bookedParentId: string;
+}
 
 const ParentSchedule = () => {
 
+    const {email} = useContext(SecurityContext);
+    const [timeBlocks, setTimeBlocks] = useState<TimeBlock[]>()
+
+
+    useEffect(() => {
+        fetch("http://" + ip + ":8080/schedule/parent/" + email)
+        // fetch("http://" + ip + ":8080/schedule/parent/coward@gmail.com")
+        .then((res) => res.json())
+        .then((data) => {
+            setTimeBlocks(data);
+        } );
+    }, []);
+
+
     return(
         <SafeAreaView>
-
-        <Text>SitterSchedule</Text>
-
-
+            <ScrollView>
+                {timeBlocks && (
+                    timeBlocks.map((block) => (
+                        <View>
+                            <Text>Start Hour:</Text>
+                            <Text>{block.startHour}</Text>
+                            <Text>End Hour</Text>
+                            <Text>{block.endHour}</Text>
+                            <Text>Date</Text>
+                            <Text>{block.dateBooked.split(":")[0].slice(0,-3)}</Text>
+                        </View>
+                    ))
+                )}
+            </ScrollView>
         </SafeAreaView>
     );
 };
